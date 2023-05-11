de987f1c4fbbb7766d849a0d12fa06850c18cf32
DreamWalker06 <kiru59@hotmail.fr>
2023-05-11 13:54:10 +0200

    Ajout des données dynamiques de la catégorie works

---
+++ FrontEnd/works.js
@@ -0,0 +1,19 @@
+fetch('http://localhost:5678/api/works')
+  .then(response => response.json())
+  .then(data => {
+    data.forEach(objet => {
+        const div = document.createElement("div");
+        const title = document.createElement("p");
+        title.textContent = objet.title;
+        const img = document.createElement("img");
+        img.src = objet.imageUrl;
+        div.appendChild(img);
+        div.appendChild(title);
+        document.querySelector('.gallery').appendChild(div);
+    })
+         console.log(data); 
+    
+  })
+  .catch(error => {
+    console.error(error); 
+  });
