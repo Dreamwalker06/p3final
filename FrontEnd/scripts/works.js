605914a702de3cd720ff0463f24ac8ee2480cc72
DreamWalker06 <kiru59@hotmail.fr>
2023-05-13 15:46:41 +0200

    Ajout de la fonctionnalité filtre

--- FrontEnd/scripts/works.js
+++ FrontEnd/scripts/works.js
@@ -1,4 +1,4 @@
-const test = []
+
 fetch('http://localhost:5678/api/works')
   .then(response => response.json())
   .then(data => {
@@ -12,31 +12,58 @@ fetch('http://localhost:5678/api/works')
         div.appendChild(img);
         div.appendChild(title);
   })
-    test = data
   })
   .catch(error => {
     console.error(error); 
   });
 
-fetch('http://localhost:5678/api/categories')
-  .then(response => response.json())
-  .then(data => {
-    data.forEach(objet => {
+  let filteredData = null;
+
+  fetch('http://localhost:5678/api/categories')
+    .then(response => response.json())
+    .then(data => {
+      const allButton = document.createElement("button");
+      document.querySelector('.filtre').appendChild(allButton);
+      allButton.textContent = "Tous";
+      allButton.addEventListener('click', () => {
+        fetch('http://localhost:5678/api/works')
+          .then(response => response.json())
+          .then(data => {
+            filteredData = null;
+            displayProjects(data);
+          });
+      });
+  
+      data.forEach(objet => {
         const button = document.createElement("button");
         document.querySelector('.filtre').appendChild(button);
         button.textContent = objet.name;
-        button.addEventListener('click', () => 
-        // correspondance entre test et objet.name avec for //
-        console.log(objet.name)
-        )
-        
-  })
-    
-  })
-  .catch(error => {
-    console.error(error); 
-  });
-
-console.log(test)
-
-
+        button.addEventListener('click', () => {
+          fetch('http://localhost:5678/api/works')
+            .then(response => response.json())
+            .then(data => {
+              filteredData = data.filter(obj => obj.categoryId === objet.id);
+              displayProjects(filteredData);
+            });
+        });
+      });
+    })
+    .catch(error => {
+      console.error(error); 
+    });
+  
+  function displayProjects(data) {
+    const gallery = document.querySelector('.gallery');
+    gallery.innerHTML = '';
+    data.forEach(objet => {
+      const div = document.createElement("div");
+      gallery.appendChild(div);
+      const title = document.createElement("p");
+      title.textContent = objet.title;
+      const img = document.createElement("img");
+      img.src = objet.imageUrl;
+      div.appendChild(img);
+      div.appendChild(title);
+    });
+  }
+  
